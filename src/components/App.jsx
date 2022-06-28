f6bbe7af201938f69555ec6dee5b354e4f86a41d
Samuel Abioye <101730798+AbioyeSamuel@users.noreply.github.com>
2022-06-28 11:57:43 +0100

    First Name and Last Name events

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  function firstNameInput(event) {
+    setFirstName(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {firstName} {lastName}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          onChange={firstNameInput}
+          name="fName"
+          placeholder="First Name"
+        />
+        <input onChange={lastNameInput} name="lName" placeholder="Last Name" />
         <button>Submit</button>
       </form>
     </div>
